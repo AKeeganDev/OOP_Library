5f7d9f3807cf2622cade23fe6b20560e25997986
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-06-14 13:19:18 -0400

    Add Comments

--- decorators.rb
+++ decorators.rb
@@ -11,12 +11,14 @@ class BaseDecorator
 end
 
 class CapitalizeDecorator < BaseDecorator
+  # returns an all uppercase name that is stripped of whitespace
   def correct_name
     @nameable.correct_name.strip.upcase
   end
 end
 
 class TrimmerDecorator < BaseDecorator
+  # returns the first 10 characters of a name that is stripped of whitespace
   def correct_name
     @nameable.correct_name.strip.slice(0, 10)
   end
